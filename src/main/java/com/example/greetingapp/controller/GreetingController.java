9059c16f13db6c6f08435193d9e3088b95080d40
Deepansh_5 <104337001+DeepanshK5@users.noreply.github.com>
2025-02-23 15:06:58 +0530

    [Deepansh]Add. Implemented UC2: Added Service Layer

--- src/main/java/com/example/greetingapp/controller/GreetingController.java
+++ src/main/java/com/example/greetingapp/controller/GreetingController.java
@@ -1,13 +1,18 @@
 package com.example.greetingapp.controller;
 
+import com.example.greetingapp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greetings")
 public class GreetingController {
 
+    @Autowired
+    private GreetingService greetingService; // Injecting the service
+
     @GetMapping("/hello")
     public String getSimpleGreeting() {
-        return "Hello, World!";
+        return greetingService.getSimpleGreeting(); // Calling the service method
     }
 }
